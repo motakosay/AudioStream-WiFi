e86ea309b519864d10337cc26384b708df1c3a51
Ironside <93154332+motakosay@users.noreply.github.com>
2025-10-17 22:27:17 +0300

    Delete python directory

--- python/audiostream_wifi_server/__init__.py
+++
@@ -1,8 +0,0 @@
-"""WiFiAudioLink Python server package
-Exposes AudioStreamer class.
-"""
-from .server import AudioStreamer
-from .discovery import DiscoveryBroadcaster
-
-
-__all__ = ["AudioStreamer", "DiscoveryBroadcaster"]
